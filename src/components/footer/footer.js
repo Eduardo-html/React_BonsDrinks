93e17c29926c52660b483702bf029209aba7a73d
Luiz Eduardo <dudumultiplo@hotmail.com>
2020-10-07 12:46:31 -0300

    first commit first react

---
+++ src/components/footer/footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    <footer>
+      <p>Resilia 2020 - Alguns direitos reservados</p>
+    </footer>
+  }
+}
+
+export default Footer;
---
+++ src/components/header/header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        <header>
+            <ol>
+                <li>Drinks</li>
+                <li>Sobre nós</li>
+                <li>Bons Drinks</li>
+                <li>Nosso Time</li>
+                <li>Contato</li>
+            </ol>
+            <img></img>
+        </header>
+    }
+}
+export default Header;
